febeca5f57acd7fc40a9326d48f0687e26096efd
Lucas Ribeiro <lucas.mr@aluno.ufop.edu.br>
2024-12-16 14:34:07 +0000

    feat: basic experiment structure

---
+++ utils_exp/experiment_facade.py
@@ -0,0 +1,167 @@
+from __future__ import annotations
+
+import logging
+from typing import TYPE_CHECKING
+
+import mlflow
+import mlflow.client
+import mlflow.experiments
+import pandas as pd
+from gluonts.dataset.pandas import PandasDataset
+from gluonts.evaluation import Evaluator
+from gluonts.evaluation.backtest import _to_dataframe
+from numpy.core.umath import arange
+
+from utils_exp.splitter import TSFMExperimentSplitter
+
+if TYPE_CHECKING:
+    from collections.abc import Iterator
+
+    from gluonts.dataset.common import Dataset
+    from gluonts.dataset.split import AbstractBaseSplitter, TestData
+    from gluonts.model.forecast import Forecast
+    from gluonts.model.predictor import Predictor
+    from pandas import DataFrame
+
+import time
+
+
+class MLExperimentFacade:
+    def __init__(
+        self,
+        experiment_name: str,
+        artifacts_path: str,
+        prediction_length: int,
+        context_lenght: int,
+        splitter: AbstractBaseSplitter | None = None,
+    ):
+        self.experiment_name = experiment_name
+        self.artifacts_path = artifacts_path
+        self.context_lenght = context_lenght
+        self.splitter = splitter or TSFMExperimentSplitter(
+            context_length=context_lenght
+        )
+        self.prediction_length = prediction_length
+        if (exp := mlflow.get_experiment_by_name(self.experiment_name)) is None:
+            self.experiment_id = mlflow.create_experiment(self.experiment_name, artifact_location=self.artifacts_path)
+        else:
+            self.experiment_id: str = exp.experiment_id
+
+    def run_experiment(
+        self, model_name: str, dataset_path, predictor: Predictor, num_samples: int
+    ) -> None:
+        mlflow.start_run(run_name=model_name, experiment_id=self.experiment_id)
+        logging.info(f"Starting experiment {model_name}")
+        mlflow.log_params(self.__dict__)
+        mlflow.log_params(
+            {
+                "model_name": model_name,
+                "dataset_path": dataset_path,
+                "num_samples": num_samples,
+            }
+        )
+
+        data = self.get_data(dataset_path)
+        logging.info(f"Data loaded from {dataset_path}")
+
+        agg_metrics, item_metrics = self.backtest_metrics(
+            data, predictor, num_samples=num_samples
+        )
+        logging.info(f"Metrics calculated")
+        for metric_name, metric_value in agg_metrics.items():
+            sanitized_metric_name = metric_name.replace("[", "_").replace("]", "_")
+            mlflow.log_metric(sanitized_metric_name, metric_value)
+        logging.info(f"Metrics logged")
+
+        item_metrics_path = f"{self.artifacts_path}/item_metrics.csv"
+        item_metrics.to_csv(item_metrics_path, index=False)
+        mlflow.log_artifact(item_metrics_path)
+        mlflow.end_run()
+
+    def get_data(self, path: str) -> Dataset:
+        df = pd.read_parquet(path)
+        df.rename(
+            columns={"y": "target", "unique_id": "item_id", "ds": "timestamp"},
+            inplace=True,
+        )
+        df.set_index(["timestamp"], inplace=True)
+        return PandasDataset.from_long_dataframe(df, item_id="item_id", freq="M")
+
+    def backtest_metrics(
+        self,
+        dataset: Dataset,
+        predictor: Predictor,
+        evaluator=Evaluator(quantiles=(0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9)),
+        num_samples: int = 100,
+    ) -> tuple[dict[str, float], DataFrame]:
+        """
+        Calculate metrics for a predictor.
+
+        This method was implemented because the default method in the GluonTS library assumes splitters that do not meet the requirements of this evaluation.
+
+        Parameters
+        ----------
+        dataset
+            Dataset to use for testing.
+        predictor
+            The predictor to test.
+        evaluator
+            Evaluator to use.
+        num_samples
+            Number of samples to use when generating sample-based forecasts. Only
+            sampling-based models will use this.
+
+        Returns
+        -------
+        A tuple of aggregate metrics and metrics per time series split.
+        """
+        forecast_it, ts_it = self.make_evaluation_predictions(
+            predictor=predictor, dataset=dataset, num_samples=num_samples
+        )
+        # Calculate time taken to make forecast
+        start_time = time.time()
+        forecast = list(forecast_it)
+        end_time = time.time()
+        logging.info(f"Time taken to transform to list of size {len(forecast)}: {end_time - start_time} seconds")
+        mlflow.log_metric("time_to_transform_forecast", end_time - start_time)
+        mlflow.log_metric("forecast_size", len(forecast))
+
+        agg_metrics, item_metrics = evaluator(ts_it, forecast)
+        return agg_metrics, item_metrics
+
+    def make_evaluation_predictions(
+        self,
+        predictor: Predictor,
+        dataset: Dataset,
+        num_samples: int,
+        distance: int | None = None,
+    ) -> tuple[Iterator[Forecast], Iterator[DataFrame]]:
+        """
+        Generate forecasts and time series from a dataset.
+
+        This method was implemented because the default method in the GluonTS library assumes splitters that do not meet the requirements of this evaluation.
+
+        Parameters
+        ----------
+        predictor
+            Predictor to use.
+        dataset
+            Dataset to generate forecasts from.
+        num_samples
+            Number of samples to use when generating sample-based forecasts.
+        distance
+            Distance between windows. The default is the context length.
+
+            Returns
+            -------
+            A tuple of forecasts and time series obtained by predicting `dataset` with `predictor`.
+        """
+        _, test_template = self.splitter.split(dataset)
+        test_data: TestData = test_template.generate_instances(
+            prediction_length=self.prediction_length,
+            distance=distance,
+        )
+        return (
+            predictor.predict(test_data.input, num_samples=num_samples),
+            map(_to_dataframe, test_data),
+        )
---
+++ utils_exp/splitter.py
@@ -0,0 +1,88 @@
+from __future__ import annotations
+
+from dataclasses import dataclass
+from typing import TYPE_CHECKING
+
+from gluonts.dataset.field_names import FieldName
+from gluonts.dataset.split import AbstractBaseSplitter, slice_data_entry
+
+if TYPE_CHECKING:
+    from collections.abc import Generator
+    from gluonts.dataset.common import DataEntry, Dataset
+
+
+@dataclass
+class TSFMExperimentSplitter(AbstractBaseSplitter):
+    """
+    A splitter that slices data to evaluate the model's performance in a time series forecasting setting.
+
+    Instead of using fixed windows to split the data, maximize the splits based on the context length while accounting for the prediction length.
+    Ensure that the available time points are sufficient both for the model's input and for evaluating the predictions.
+
+    Parameters
+    ----------
+    offset
+        Offset determining where the training data ends.
+        A positive offset indicates how many observations since the start of
+        each series should be in the training slice; a negative offset
+        indicates how many observations before the end of each series should
+        be excluded from the training slice.
+    """
+
+    offset: int = 0
+    context_length: int = 32
+
+    def training_entry(self, entry: DataEntry) -> DataEntry:
+        # TODO: Verificar a real necessidade
+        return slice_data_entry(entry, slice(None, self.offset))
+
+    def test_pair(
+        self, entry: DataEntry, prediction_length: int, offset: int = 0
+    ) -> tuple[DataEntry, DataEntry]:
+        if self.offset < 0:
+            offset += entry[FieldName.TARGET].shape[-1]
+
+        input_slice = slice(max(0, offset - self.context_length), offset)
+        label_slice = slice(offset, offset + prediction_length)
+
+        return (
+            slice_data_entry(entry, input_slice, prediction_length=prediction_length),
+            slice_data_entry(entry, label_slice, prediction_length=prediction_length),
+        )
+
+    def generate_test_pairs(
+        self,
+        dataset: Dataset,
+        prediction_length: int,
+        windows: int = 1,
+        distance: int | None = None,
+        max_history: int | None = None,
+    ) -> Generator[tuple[DataEntry, DataEntry], None, None]:
+        """
+        Generate test pairs from a dataset.
+
+        Parameters
+        ----------
+        dataset
+            Dataset to generate test pairs from.
+        prediction_length
+            Prediction length.
+        windows
+            This parameter is not used in this implementation.
+        distance
+            Distance between windows. Thw default is the context length.
+        max_history
+            This parameter is not used in this implementation.
+        """
+        if distance is None:
+            distance = self.context_length
+
+        for entry in dataset:
+            total_length = entry[FieldName.TARGET].shape[-1]
+            current_offset = self.context_length
+            while current_offset + prediction_length <= total_length:
+                test = self.test_pair(
+                    entry, prediction_length=prediction_length, offset=current_offset
+                )
+                yield test[0], test[1]
+                current_offset += distance
