fe91fb3be988224d08d6950fb904c399b288ccd3
Lucas Ribeiro <lucas.mr@aluno.ufop.edu.br>
2024-12-17 19:20:07 +0000

    feat: add moirai experiment

---
+++ experiment_moirai/__main__.py
@@ -0,0 +1,81 @@
+import logging
+
+from uni2ts.model.moirai import MoiraiForecast, MoiraiModule
+from uni2ts.model.moirai_moe import MoiraiMoEForecast, MoiraiMoEModule
+
+from utils_exp import Args, MLExperimentFacade, get_parser
+
+# Configure logging
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__package__)
+
+class MoiraiArgs(Args):
+    patch_size: int
+    batch_size: int
+
+def main():
+    logger.info("Initializing experiment")
+    parser = get_parser()
+
+    parser.add_argument(
+        "--patch_size",
+        choices=["auto", 8, 16, 32, 64, 128],
+        default="auto",
+        help="Patch size for Moirai model",
+    )
+
+    parser.add_argument(
+        "--batch_size",
+        type=int,
+        default=32,
+        help="Batch size for Moirai model",
+    )
+
+    args = parser.parse_args(namespace=MoiraiArgs)
+
+    exp = MLExperimentFacade(
+        experiment_name=args.experiment_name,
+        artifacts_path=args.artifacts_path,
+        prediction_length=args.prediction_length,
+        context_length=args.context_length,
+    )
+
+
+    # Prepare pre-trained model by downloading model weights from huggingface hub
+    if args.model_name == "moirai":
+        model = MoiraiForecast(
+            module=MoiraiModule.from_pretrained(f"Salesforce/moirai-1.1-R-{args.model_size}"),
+            prediction_length=args.prediction_length,
+            context_length=args.context_length,
+            patch_size=args.patch_size,
+            num_samples=args.num_samples,
+            target_dim=1,
+            feat_dynamic_real_dim=0,
+            past_feat_dynamic_real_dim=0,
+        )
+    elif args.model_name == "moirai-moe":
+        model = MoiraiMoEForecast(
+            module=MoiraiMoEModule.from_pretrained(f"Salesforce/moirai-moe-1.0-R-{args.model_size}"),
+            prediction_length=args.prediction_length,
+            context_length=args.context_length,
+            patch_size=args.patch_size,
+            num_samples=args.num_samples,
+            target_dim=1,
+            feat_dynamic_real_dim=0,
+            past_feat_dynamic_real_dim=0,
+        )
+    else:
+        raise ValueError(f"Model {args.model_name} not supported")
+    logger.info(f"Creating predicor")
+    predictor = model.create_predictor(batch_size=args.batch_size)
+
+    exp.run_experiment(
+        model_name=args.model_name,
+        dataset_path=args.data_path,
+        predictor=predictor,
+        num_samples=args.num_samples,
+    )
+
+
+if __name__ == "__main__":
+    main()
