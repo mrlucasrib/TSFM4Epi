bf4bb2221983a414c117b016a3037a4127360f73
Lucas Ribeiro <lucas.mr@aluno.ufop.edu.br>
2025-01-07 16:20:11 +0000

    feat: experiment lagllama

--- experiment_lagllama/__main__.py
+++ experiment_lagllama/__main__.py
@@ -1,3 +1,4 @@
+import logging
 import torch
 from lag_llama.gluon.estimator import LagLlamaEstimator
 
@@ -5,21 +6,26 @@ from utils_exp.cli import get_parser
 from utils_exp.dummy_module import create_dummy_gluonts_torch_module
 from utils_exp.experiment_facade import MLExperimentFacade
 
+# Configure logging
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__package__)
+
 if __name__ == "__main__":
+    logger.info("Initializing experiment")
+    parser = get_parser()
+    args = parser.parse_args()
 
     create_dummy_gluonts_torch_module()
     device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
-    prediction_length = 24
 
-    ckpt = torch.load(
-        "content/lag-llama/lag-llama.ckpt", map_location=device, weights_only=False
-    )  # Uses GPU since in this Colab we use a GPU.
+    prediction_length = args.prediction_length
+
+    ckpt = torch.load(args.model_path, map_location=device, weights_only=False)
     estimator_args = ckpt["hyper_parameters"]["model_kwargs"]
     estimator = LagLlamaEstimator(
-        ckpt_path="content/lag-llama/lag-llama.ckpt",
+        ckpt_path=args.model_path,
         prediction_length=prediction_length,
-        context_length=32,  # Should not be changed; this is what the released Lag-Llama model was trained with
-        # estimator args
+        context_length=args.context_length,
         input_size=estimator_args["input_size"],
         n_layer=estimator_args["n_layer"],
         n_embd_per_head=estimator_args["n_embd_per_head"],
@@ -27,7 +33,7 @@ if __name__ == "__main__":
         scaling=estimator_args["scaling"],
         time_feat=estimator_args["time_feat"],
         batch_size=1,
-        num_parallel_samples=100,
+        num_parallel_samples=args.num_samples,
         device=device,
     )
 
@@ -35,45 +41,15 @@ if __name__ == "__main__":
     transformation = estimator.create_transformation()
     predictor = estimator.create_predictor(transformation, lightning_module)
 
-    if __name__ == "__main__":
-        parser = get_parser()
-        args = parser.parse_args()
-
-        create_dummy_gluonts_torch_module()
-        device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
-
-        prediction_length = args.prediction_length
-
-        ckpt = torch.load(args.ckpt_path, map_location=device, weights_only=False)
-        estimator_args = ckpt["hyper_parameters"]["model_kwargs"]
-        estimator = LagLlamaEstimator(
-            ckpt_path=args.model_path,
-            prediction_length=prediction_length,
-            context_length=args.context_length,
-            input_size=estimator_args["input_size"],
-            n_layer=estimator_args["n_layer"],
-            n_embd_per_head=estimator_args["n_embd_per_head"],
-            n_head=estimator_args["n_head"],
-            scaling=estimator_args["scaling"],
-            time_feat=estimator_args["time_feat"],
-            batch_size=1,
-            num_parallel_samples=args.num_samples,
-            device=device,
-        )
-
-        lightning_module = estimator.create_lightning_module()
-        transformation = estimator.create_transformation()
-        predictor = estimator.create_predictor(transformation, lightning_module)
-
-        exp = MLExperimentFacade(
-            experiment_name=args.experiment_name,
-            artifacts_path=args.artifacts_path,
-            prediction_length=prediction_length,
-            context_length=args.context_length,
-        )
-        exp.run_experiment(
-            args.model_name,
-            args.data_path,
-            predictor,
-            args.num_samples,
-        )
+    exp = MLExperimentFacade(
+        experiment_name=args.experiment_name,
+        artifacts_path=args.artifacts_path,
+        prediction_length=prediction_length,
+        context_length=args.context_length,
+    )
+    exp.run_experiment(
+        args.model_name,
+        args.data_path,
+        predictor,
+        args.num_samples,
+    )
---
+++ experiment_lagllama/setup.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+huggingface-cli download time-series-foundation-models/Lag-Llama lag-llama.ckpt --local-dir /lag-llama
