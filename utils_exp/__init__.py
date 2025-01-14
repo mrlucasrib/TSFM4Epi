2876b7bd224c71aa09deaeae433d95c3d4334d64
Lucas Ribeiro <lucas.mr@aluno.ufop.edu.br>
2025-01-14 18:35:33 +0000

    chore: ignore unecessary warnning

--- utils_exp/__init__.py
+++ utils_exp/__init__.py
@@ -1,4 +1,11 @@
+import warnings
+
 from utils_exp.cli import Args, get_parser
 from utils_exp.experiment_facade import MLExperimentFacade
+
+
+warnings.filterwarnings("ignore",
+                        category=FutureWarning,
+                        message="'M' is deprecated and will be removed in a future version, please use 'ME' instead.")
 
 __all__ = ["MLExperimentFacade", "get_parser", "Args"]
